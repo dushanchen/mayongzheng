9b19ef2267f30ecf9a4c3ce8e3b1c7fbd8112ebe
杜膳辰 <dsc@macdeMacBook-Pro-5.local>
2019-08-24 21:39:55 +0800

    gogo

--- laoma/build.sh
+++ laoma/build.sh
@@ -1,8 +1,8 @@
-cd /root/project/YongDan
+cd /root/project/mayongzheng/laoma
 git fetch 
 git merge origin/master
 
-cd project
+cd laoma
 python3 manage.py migrate --settings=project.settings_prod
 python3 manage.py collectstatic --settings=project.settings_prod
 
